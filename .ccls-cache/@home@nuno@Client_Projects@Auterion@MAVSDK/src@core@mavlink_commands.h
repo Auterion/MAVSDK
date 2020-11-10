95d2ea51c064e47a545da08e63f870b3b5f5b493
TSC21 <n.marques21@hotmail.com>
2020-11-10 12:59:07 +0100

    core: mavlink_commands: send ACK with result according to the callback result

--- .ccls-cache/@home@nuno@Client_Projects@Auterion@MAVSDK/src@core@mavlink_commands.h
+++ .ccls-cache/@home@nuno@Client_Projects@Auterion@MAVSDK/src@core@mavlink_commands.h
@@ -3,14 +3,11 @@
 #include "mavlink_include.h"
 #include "locked_queue.h"
 #include "global_include.h"
-#include <cmath>
 #include <cstdint>
 #include <string>
 #include <functional>
 #include <mutex>
 #include <optional>
-#include <unordered_map>
-#include <variant>
 
 namespace mavsdk {
 
@@ -40,7 +37,7 @@ public:
         uint8_t target_component_id{0};
         MAV_FRAME frame = MAV_FRAME_GLOBAL_RELATIVE_ALT;
         uint16_t command{0};
-        bool current = false;
+        bool current = 0;
         bool autocontinue = false;
         // Most of the "Reserved" values in MAVLink spec are NAN.
         struct Params {
@@ -52,24 +49,35 @@ public:
             int32_t y = 0;
             float z = NAN;
         } params{};
+
+        // In some cases "Reserved" value could be "0".
+        // This utility method can be used in such case.
+        // TODO: rename to set_all
+        static void set_as_reserved(Params& params, float reserved_value = NAN)
+        {
+            params.param1 = reserved_value;
+            params.param2 = reserved_value;
+            params.param3 = reserved_value;
+            params.param4 = reserved_value;
+            params.x = 0;
+            params.y = 0;
+            params.z = reserved_value;
+        }
     };
 
     struct CommandLong {
-        CommandLong() = delete;
-        explicit CommandLong(const SystemImpl& system_impl);
-
         uint8_t target_system_id{0};
         uint8_t target_component_id{0};
         uint16_t command{0};
         uint8_t confirmation = 0;
         struct Params {
-            float param1;
-            float param2;
-            float param3;
-            float param4;
-            float param5;
-            float param6;
-            float param7;
+            float param1 = NAN;
+            float param2 = NAN;
+            float param3 = NAN;
+            float param4 = NAN;
+            float param5 = NAN;
+            float param6 = NAN;
+            float param7 = NAN;
         } params{};
 
         // TODO: rename to set_all
@@ -88,8 +96,8 @@ public:
     Result send_command(const CommandInt& command);
     Result send_command(const CommandLong& command);
 
-    void queue_command_async(const CommandInt& command, const CommandResultCallback& callback);
-    void queue_command_async(const CommandLong& command, const CommandResultCallback& callback);
+    void queue_command_async(const CommandInt& command, CommandResultCallback callback);
+    void queue_command_async(const CommandLong& command, CommandResultCallback callback);
 
     void do_work();
 
@@ -100,64 +108,25 @@ public:
     const MavlinkCommandSender& operator=(const MavlinkCommandSender&) = delete;
 
 private:
-    // The std::monostate is required to work around the fact that
-    // the default ctor of CommandLong and CommandInt is ill-defined.
-    using Command = std::variant<std::monostate, CommandLong, CommandInt>;
-
-    struct CommandIdentification {
-        uint32_t maybe_param1{0}; // only for commands where this matters
-        uint16_t command{0};
-        uint8_t target_system_id{0};
-        uint8_t target_component_id{0};
-
-        bool operator==(const CommandIdentification& other) const
-        {
-            return maybe_param1 == other.maybe_param1 && command == other.command &&
-                   target_system_id == other.target_system_id &&
-                   target_component_id == other.target_component_id;
-        }
-
-        bool operator!=(const CommandIdentification& other) const { return !(*this == other); }
-    };
-
     struct Work {
-        Command command;
-        CommandIdentification identification{};
+        int retries_to_do{3};
+        double timeout_s{0.5};
+        uint16_t mavlink_command{0};
+        bool already_sent{false};
+        mavlink_message_t mavlink_message{};
         CommandResultCallback callback{};
         dl_time_t time_started{};
-        void* timeout_cookie = nullptr;
-        double timeout_s{0.5};
-        int retries_to_do{3};
-        bool already_sent{false};
     };
 
-    template<typename CommandType>
-    CommandIdentification identification_from_command(const CommandType& command)
-    {
-        CommandIdentification identification{};
-
-        identification.command = command.command;
-        if (command.command == MAV_CMD_REQUEST_MESSAGE ||
-            command.command == MAV_CMD_SET_MESSAGE_INTERVAL) {
-            identification.maybe_param1 = static_cast<uint32_t>(std::lround(command.params.param1));
-        }
-        identification.target_system_id = command.target_system_id;
-        identification.target_component_id = command.target_component_id;
-
-        return identification;
-    }
-
     void receive_command_ack(mavlink_message_t message);
-    void receive_timeout(const CommandIdentification& identification);
+    void receive_timeout();
 
     void call_callback(const CommandResultCallback& callback, Result result, float progress);
-
-    mavlink_message_t create_mavlink_message(const Command& command);
 
     SystemImpl& _parent;
     LockedQueue<Work> _work_queue{};
 
-    bool _command_debugging{false};
+    void* _timeout_cookie = nullptr;
 };
 
 class MavlinkCommandReceiver {
@@ -184,7 +153,7 @@ public:
         uint8_t origin_component_id{0};
         MAV_FRAME frame = MAV_FRAME_GLOBAL_RELATIVE_ALT;
         uint16_t command{0};
-        bool current{false};
+        bool current{0};
         bool autocontinue{false};
         // Most of the "Reserved" values in MAVLink spec are NAN.
         struct Params {
@@ -197,7 +166,7 @@ public:
             float z = NAN;
         } params{};
 
-        explicit CommandInt(const mavlink_message_t& message)
+        CommandInt(const mavlink_message_t& message)
         {
             mavlink_command_int_t command_int;
             mavlink_msg_command_int_decode(&message, &command_int);
@@ -237,7 +206,7 @@ public:
             float param7 = NAN;
         } params{};
 
-        explicit CommandLong(const mavlink_message_t& message)
+        CommandLong(const mavlink_message_t& message)
         {
             mavlink_command_long_t command_long;
             mavlink_msg_command_long_decode(&message, &command_long);
@@ -264,9 +233,9 @@ public:
         std::function<std::optional<mavlink_message_t>(const CommandLong&)>;
 
     void register_mavlink_command_handler(
-        uint16_t cmd_id, const MavlinkCommandIntHandler& callback, const void* cookie);
+        uint16_t cmd_id, MavlinkCommandIntHandler callback, const void* cookie);
     void register_mavlink_command_handler(
-        uint16_t cmd_id, const MavlinkCommandLongHandler& callback, const void* cookie);
+        uint16_t cmd_id, MavlinkCommandLongHandler callback, const void* cookie);
 
     void unregister_mavlink_command_handler(uint16_t cmd_id, const void* cookie);
     void unregister_all_mavlink_command_handlers(const void* cookie);
