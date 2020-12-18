3658165f9d99f3c41eb73f4cbc57e2f26974bc01
TSC21 <n.marques21@hotmail.com>
2020-12-18 20:00:34 +0000

    add a separate script to build and push Auterion specific docker images
    Can be useful to be used by a separate repo

---
+++ docker/build_and_push_auterion_docker_images.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Specific to APX4 releases
+
+set -e
+
+docker build --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" -f Dockerfile-Ubuntu-20.04-APX4-SITL-v2.1 -t auterion/mavsdk-ubuntu-20.04-apx4-sitl-v2.1 .
+docker build --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" -f Dockerfile-Ubuntu-20.04-APX4-SITL-develop -t auterion/mavsdk-ubuntu-20.04-apx4-sitl-develop .
+
+docker push auterion/mavsdk-ubuntu-20.04-apx4-sitl-v2.1:latest
+docker push auterion/mavsdk-ubuntu-20.04-apx4-sitl-develop:latest
