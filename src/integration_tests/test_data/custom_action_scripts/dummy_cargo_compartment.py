44fab8eeb442f1d57fe2ba8efc9e04bc3f0acdc3
TSC21 <n.marques21@hotmail.com>
2020-12-23 13:25:42 +0000

    intergration_tests: custom_action: add better example tests and dummy scripts

---
+++ src/integration_tests/test_data/custom_action_scripts/dummy_cargo_compartment.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+
+"""
+Dummy cargo compartment control script
+"""
+
+def cargo_control(signal):
+    if (signal == "open"):
+        print("\n - Opening cargo compartment latch...\n");
+    elif (signal == "close"):
+        print("\n - Closing cargo compartment latch...\n");
+
+def main():
+    cargo_control(sys.argv[1])
+
+
+main()
---
+++ src/integration_tests/test_data/custom_action_scripts/dummy_safe_landing.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import time
+
+"""
+Dummy safe landing script
+"""
+
+def main():
+    print("\n - Safe landing started!\n");
+    print(" - Executing area verification while descending...\n");
+
+    time.sleep(4)
+
+    print(" - Safe land executed.\n");
+
+main()
