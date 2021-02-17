4515adfe61917fad390ee14f5ddef254da1a0491
TSC21 <n.marques21@hotmail.com>
2021-02-17 11:06:43 +0100

    integration_tests: obstacle_avoidance_control: add dummy obstacle avoidance script and usage

---
+++ src/integration_tests/test_data/obstacle_avoidance_dummy.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import multiprocessing
+import os
+import sys
+
+from functools import partial
+from signal import pause, signal, SIGINT, SIGTERM
+from threading import Thread
+from time import sleep
+
+"""
+Dummy obstacle avoidance script
+"""
+
+cycle_counter = 0
+stop_signal = False
+
+def run():
+    global cycle_counter
+    while not stop_signal:
+        cycle_counter += 1
+        sleep(0.1)
+
+def signal_handler(th, sig, frame):
+    global stop_signal
+    stop_signal = True
+    th.join()
+
+def main():
+    th = Thread(target=run, args=())
+    th.start()
+
+    PID = str(os.getpid())
+    with open('obs_avoid_service.pid', 'w') as file:
+        file.write(PID)
+
+    signal(SIGINT, partial(signal_handler, th))
+    signal(SIGTERM, partial(signal_handler, th))
+
+    print("\nObstacle avoidance service started!\n");
+
+    pause()
+
+    print("\nObstacle avoidance service stopped! Ran for {} cycles.".format(cycle_counter));
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
