3546e885837eebb88a56e40306c83dff8871eebc
TSC21 <n.marques21@hotmail.com>
2021-03-01 10:24:21 +0100

    add APX4-SITL-v2.2 Dockerfile and tests against APX4 2.2

--- docker/build_and_push_auterion_docker_images.sh
+++ docker/build_and_push_auterion_docker_images.sh
@@ -3,8 +3,10 @@
 
 set -e
 
+docker build --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" -f Dockerfile-Ubuntu-20.04-APX4-SITL-v2.2 -t auterion/mavsdk-ubuntu-20.04-apx4-sitl-v2.2 .
 docker build --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" -f Dockerfile-Ubuntu-20.04-APX4-SITL-v2.1 -t auterion/mavsdk-ubuntu-20.04-apx4-sitl-v2.1 .
 docker build --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" -f Dockerfile-Ubuntu-20.04-APX4-SITL-develop -t auterion/mavsdk-ubuntu-20.04-apx4-sitl-develop .
 
+docker push auterion/mavsdk-ubuntu-20.04-apx4-sitl-v2.2:latest
 docker push auterion/mavsdk-ubuntu-20.04-apx4-sitl-v2.1:latest
 docker push auterion/mavsdk-ubuntu-20.04-apx4-sitl-develop:latest
