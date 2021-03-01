28683c92b67cdccf80801106858affaa4c05685a
TSC21 <n.marques21@hotmail.com>
2021-03-01 09:51:22 +0100

    fix calibration API due to proto update

--- src/mavsdk/plugins/calibration/calibration_impl.cpp
+++ src/mavsdk/plugins/calibration/calibration_impl.cpp
@@ -258,6 +258,7 @@ Calibration::Result CalibrationImpl::cancel()
     command.target_component_id = target_component_id;
     _parent->send_command_async(command, nullptr);
 
+<<<<<<< HEAD
     auto prom = std::promise<Calibration::Result>();
     auto fut = prom.get_future();
     _parent->send_command_async(
@@ -270,6 +271,9 @@ Calibration::Result CalibrationImpl::cancel()
         });
 
     return fut.get();
+=======
+    return Calibration::Result::Success;
+>>>>>>> fix calibration API due to proto update
 }
 
 void CalibrationImpl::command_result_callback(
