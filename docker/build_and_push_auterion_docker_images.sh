b4e7de9f00515fd8960cd3b9eae0806b1709529d
TSC21 <n.marques21@hotmail.com>
2021-09-20 17:02:01 +0200

    docker: add 2.3 image and use id_ed25519 instead of id_rsa

--- docker/build_and_push_auterion_docker_images.sh
+++ docker/build_and_push_auterion_docker_images.sh
@@ -3,10 +3,12 @@
 
 set -e
 
-docker build --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" -f Dockerfile-Ubuntu-20.04-APX4-SITL-v2.2 -t auterion/mavsdk-ubuntu-20.04-apx4-sitl-v2.2 .
-docker build --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" -f Dockerfile-Ubuntu-20.04-APX4-SITL-v2.1 -t auterion/mavsdk-ubuntu-20.04-apx4-sitl-v2.1 .
-docker build --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" -f Dockerfile-Ubuntu-20.04-APX4-SITL-develop -t auterion/mavsdk-ubuntu-20.04-apx4-sitl-develop .
+docker build --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_ed25519)" -f Dockerfile-Ubuntu-20.04-APX4-SITL-v2.3 -t auterion/mavsdk-ubuntu-20.04-apx4-sitl-v2.3 .
+docker build --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_ed25519)" -f Dockerfile-Ubuntu-20.04-APX4-SITL-v2.2 -t auterion/mavsdk-ubuntu-20.04-apx4-sitl-v2.2 .
+docker build --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_ed25519)" -f Dockerfile-Ubuntu-20.04-APX4-SITL-v2.1 -t auterion/mavsdk-ubuntu-20.04-apx4-sitl-v2.1 .
+docker build --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_ed25519)" -f Dockerfile-Ubuntu-20.04-APX4-SITL-develop -t auterion/mavsdk-ubuntu-20.04-apx4-sitl-develop .
 
+docker push auterion/mavsdk-ubuntu-20.04-apx4-sitl-v2.3:latest
 docker push auterion/mavsdk-ubuntu-20.04-apx4-sitl-v2.2:latest
 docker push auterion/mavsdk-ubuntu-20.04-apx4-sitl-v2.1:latest
 docker push auterion/mavsdk-ubuntu-20.04-apx4-sitl-develop:latest
